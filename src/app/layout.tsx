baa20efcec7b19bfacf118a5fe02bd855072ebbd
tebellomolete <ttmolete@gmail.com>
2026-06-30 09:43:50 +0200

    feat: install sonner and integrate Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "./providers";
 import NavLinks from "@/components/NavLinks";
 import Link from "next/link";
 import { auth } from "@/auth";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default async function RootLayout({
           <Providers>
             {children}
           </Providers>
+          <Toaster position="bottom-right" richColors />
         </div>
       </body>
     </html>
